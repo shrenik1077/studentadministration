bfc7bd1df34b7b2b8aeeb167bcf97e0ddaa6d10b
Shrenik <Shrenik@ExamServer>
2020-05-03 15:15:39 +0530

    Step 1 Commit to perform CRUD operations through Spring Data Rest

---
+++ demo/demo/src/main/java/com/shrenik/StudentAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.shrenik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAdministrationApplication.class, args);
+	}
+
+}
